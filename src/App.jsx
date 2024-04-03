c65d2ce6a98f3f4347f54f26eaec9ea80e5d7439
Abdurizo <abdurizo22@gmail.com>
2024-04-03 17:30:47 +0500

    реализовал фильтр

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ class App extends Component {
         { name: 'John', salary: 800, rise: true, increase: false, id: 1 },
         { name: 'Alex', salary: 5000, rise: false, increase: true, id: 2 },
         { name: 'Gloriya', salary: 1000, rise: false, increase: false, id: 3 },
-      ]
+      ],
+      term: '',
     }
     this.maxId = 4
   }
@@ -42,7 +43,7 @@ class App extends Component {
       }
     })
   }
-  onToggleProp = (id,prop) => {
+  onToggleProp = (id, prop) => {
     // this.setState(({ data }) => {
     //   const index = data.findIndex(elem => elem.id === id);
     //   const old = data[index];
@@ -54,26 +55,39 @@ class App extends Component {
     // })
     this.setState(({ data }) => ({
       data: data.map(item => {
-        if(item.id === id){
+        if (item.id === id) {
           return { ...item, [prop]: !item[prop] }
         }
         return item;
       })
     }))
   }
- 
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter(item => {
+      return item.name.indexOf(term) > -1
+    })
+  }
+onUpdateSearch = (term) =>{
+this.setState({term});
+}
+
   render() {
+    const { data, term } = this.state;
     const employees = this.state.data.length;
     const increase = this.state.data.filter(item => item.increase).length;
+    const visibleData = this.searchEmp(data,term)
     return (
       <div className="container" >
         <Info employees={employees} increase={increase} />
         <div className="search-panel">
-          <SearchPanel />
+          <SearchPanel onUdateSearch={this.onUpdateSearch} />
           <Filter />
         </div>
         <EmployersList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp} />
         <EmployersAddForm onAdd={this.handleAddItem} />
--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -1,13 +1,28 @@
-// import React from 'react'
-import  './SearchPanel.css'
-function SearchPanel() {
-  return (
-    <input
-      type="text"
-      className="form-control search-input"
-      placeholder="Найти сотрудника"
-    />
-  )
+import { Component } from 'react';
+import './SearchPanel.css'
+class SearchPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: ''
+    }
+  }
+  handleUpdateSearch = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onUdateSearch(term);
+  }
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control search-input"
+        placeholder="Найти сотрудника"
+        value={this.state.term}
+        onChange={this.handleUpdateSearch}
+      />
+    )
+  }
 }
 
 export default SearchPanel
